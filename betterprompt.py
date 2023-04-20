1c7dcecfe7eea79a2c4e46bd0ad8e66dad7efeec
kylinkang001 <kylinkang001@gmail.com>
2023-04-20 07:35:14 +0000

    pythonprompt

---
+++ betterprompt.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import betterprompt
+
+prompt = "This is a sample prompt"
+
+perplexity = betterprompt.calculate_perplexity(prompt)
+
+print(perplexity) # Outputs the perplexity of the text
